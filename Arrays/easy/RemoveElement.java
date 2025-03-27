b34563186d0fce7d6de52647fecded473d3ddada
--unset <nvnpanwar123@gmail.com>
2025-03-28 01:28:36 +0530

    remove-element

---
+++ Arrays/easy/RemoveElement.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/remove-element
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            if(nums[i] == val){
+                nums[i] = nums[j];
+                nums[j] = val;
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return j+1;
+    }
+}
