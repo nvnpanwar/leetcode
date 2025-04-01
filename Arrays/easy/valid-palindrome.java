0ddb705868709abd4307d0546ea165c27ea9098e
--unset <nvnpanwar123@gmail.com>
2025-04-02 01:27:00 +0530

    valid-palindrome

---
+++ Arrays/easy/valid-palindrome.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        s=s.toLowerCase();
+
+        for(int i = 0;i<s.length() / 2;i++){
+            if(s.charAt(i) != s.charAt(s.length() - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
