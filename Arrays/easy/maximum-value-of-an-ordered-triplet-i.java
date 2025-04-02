1b3b2ee390d2d4e0d5dfd487b34f9cfff872180c
--unset <nvnpanwar123@gmail.com>
2025-04-02 14:39:20 +0530

    update

--- Arrays/easy/maximum-value-of-an-ordered-triplet-i.java
+++ Arrays/easy/maximum-value-of-an-ordered-triplet-i.java
@@ -1,23 +1,42 @@
 // https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/description
 
-
 class Solution {
     public long maximumTripletValue(int[] nums) {
         long ans = 0;
-        int[] max = new int[nums.length];
+        int[] leftMax = new int[nums.length];
+        int[] rightMax = new int[nums.length];
         for(int i = nums.length - 1;i>0;i--){
-            max[i-1] = Math.max(max[i], nums[i]); 
+            rightMax[i-1] = Math.max(rightMax[i], nums[i]); 
         }
-        for(int i =0;i<nums.length - 2;i++ ){
-            for(int j = i+1;j<nums.length -1;j++){
-                long sub = nums[i]-nums[j];
-                long num = max[j];
-                ans = Math.max(ans, sub * num);
-            }
+        for(int i = 1;i < nums.length ;i++){
+            leftMax[i] = Math.max(leftMax[i - 1], nums[i-1]); 
+        }
+        for(int j = 1;j<nums.length -1;j++){
+            long sub = leftMax[j]-nums[j];
+            long num = rightMax[j];
+            ans = Math.max(ans, sub * num);
         }
         return ans;
     }
 }
+
+// class Solution {
+//     public long maximumTripletValue(int[] nums) {
+//         long ans = 0;
+//         int[] max = new int[nums.length];
+//         for(int i = nums.length - 1;i>0;i--){
+//             max[i-1] = Math.max(max[i], nums[i]); 
+//         }
+//         for(int i =0;i<nums.length - 2;i++ ){
+//             for(int j = i+1;j<nums.length -1;j++){
+//                 long sub = nums[i]-nums[j];
+//                 long num = max[j];
+//                 ans = Math.max(ans, sub * num);
+//             }
+//         }
+//         return ans;
+//     }
+// }
 
 // class Solution {
 //     public long maximumTripletValue(int[] nums) {
