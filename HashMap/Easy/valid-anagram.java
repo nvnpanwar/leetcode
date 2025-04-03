7ed70dbcbefae963e94c13f3518c707c84ab38ab
--unset <nvnpanwar123@gmail.com>
2025-04-04 03:03:46 +0530

    update

--- HashMap/Easy/valid-anagram.java
+++ HashMap/Easy/valid-anagram.java
@@ -2,19 +2,39 @@
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
-        if(s.length() != t.length()){
+        int[] arr = new int[26];
+        if(s.length()!=t.length()){
             return false;
         }
+        Arrays.fill(arr, 0);
         for(int i=0;i<s.length();i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i) , 0) - 1);
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
-        for(Map.Entry<Character, Integer> entry: map.entrySet()){
-            if(entry.getValue() != 0){
+        for(int i = 0;i<26;i++){
+            if(arr[i] != 0){
                 return false;
             }
         }
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         Map<Character, Integer> map = new HashMap<>();
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+//         for(int i=0;i<s.length();i++){
+//             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+//             map.put(t.charAt(i), map.getOrDefault(t.charAt(i) , 0) - 1);
+//         }
+//         for(Map.Entry<Character, Integer> entry: map.entrySet()){
+//             if(entry.getValue() != 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
