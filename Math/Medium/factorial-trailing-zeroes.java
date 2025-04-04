5265d23bf2150ed107386aded2f1b91b4d000288
--unset <nvnpanwar123@gmail.com>
2025-04-04 13:33:12 +0530

    update

--- Math/Medium/factorial-trailing-zeroes.java
+++ Math/Medium/factorial-trailing-zeroes.java
@@ -3,14 +3,26 @@
 class Solution {
     public int trailingZeroes(int n) {
         int zero = 0;
-        for(int i =1;i<=n;i++){
-            int temp = i;
-            while(temp % 5 == 0){
-                temp = temp/5;
-                zero++;
-            }
-
+        while(n != 0){
+            zero = zero + n/5;
+            n = n/5;
         }
+
         return zero;
     }
 }
+
+// class Solution {
+//     public int trailingZeroes(int n) {
+//         int zero = 0;
+//         for(int i =1;i<=n;i++){
+//             int temp = i;
+//             while(temp % 5 == 0){
+//                 temp = temp/5;
+//                 zero++;
+//             }
+
+//         }
+//         return zero;
+//     }
+// }
