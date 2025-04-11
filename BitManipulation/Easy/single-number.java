90d7ae086612979ffd632534c18f85c2a78021a8
--unset <nvnpanwar123@gmail.com>
2025-04-11 17:17:42 +0530

    single-number

---
+++ BitManipulation/Easy/single-number.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/single-number
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int ele: nums){
+            ans = ans ^ ele;
+        }
+        return ans;
+    }
+}
