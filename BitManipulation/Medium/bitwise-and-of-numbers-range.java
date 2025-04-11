f1499aabfddaf4a8f5b7154a6539579c19eee564
--unset <nvnpanwar123@gmail.com>
2025-04-11 23:08:59 +0530

    bitwise-and-of-numbers-range

---
+++ BitManipulation/Medium/bitwise-and-of-numbers-range.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/bitwise-and-of-numbers-range
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+        while(left != right){
+            left = left >> 1;
+            right = right >> 1;
+            count++;
+        }
+        return left << count;
+    }
+}
