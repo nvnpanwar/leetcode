3c559b4c2ec22e61a896d66dbc838a7426de8799
--unset <nvnpanwar123@gmail.com>
2025-04-13 03:56:41 +0530

    update

--- BinarySearch/Medium/search-a-2d-matrix.java
+++ BinarySearch/Medium/search-a-2d-matrix.java
@@ -3,26 +3,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int left = 0;
-        int right = matrix.length;
-        while(left < right){
+        int right = matrix.length - 1;
+        int ans = -1;
+        while(left <= right){
             int mid = (left + right)/2;
             if(target == matrix[mid][0]){
                 return true;
             }
             else if(target > matrix[mid][0]){
+                ans = mid;
                 left = mid+1;
             }
             else{
-                right = mid;
+                right = mid - 1;
             }
         }
-        if(left == 0 && matrix[left][0] > target){
+        if(ans == -1){
             return false;
         }
-        int row = left - 1;
+        int row = ans;
         left = 0;
-        right = matrix[row].length;
-        while(left < right){
+        right = matrix[row].length - 1;
+        while(left <= right){
             int mid = (left + right)/2;
             if(target == matrix[row][mid]){
                 return true;
@@ -31,9 +33,47 @@ class Solution {
                 left = mid+1;
             }
             else{
-                right = mid;
+                right = mid - 1;
             }
         }
         return false;
     }
 }
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int left = 0;
+//         int right = matrix.length;
+//         while(left < right){
+//             int mid = (left + right)/2;
+//             if(target == matrix[mid][0]){
+//                 return true;
+//             }
+//             else if(target > matrix[mid][0]){
+//                 left = mid+1;
+//             }
+//             else{
+//                 right = mid;
+//             }
+//         }
+//         if(left == 0 && matrix[left][0] > target){
+//             return false;
+//         }
+//         int row = left - 1;
+//         left = 0;
+//         right = matrix[row].length;
+//         while(left < right){
+//             int mid = (left + right)/2;
+//             if(target == matrix[row][mid]){
+//                 return true;
+//             }
+//             else if(target > matrix[row][mid]){
+//                 left = mid+1;
+//             }
+//             else{
+//                 right = mid;
+//             }
+//         }
+//         return false;
+//     }
+// }
