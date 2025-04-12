ef3235ec7bea6123fe0f331dc4a6e5472967e384
--unset <nvnpanwar123@gmail.com>
2025-04-13 03:49:38 +0530

    update

--- BinarySearch/Easy/search-insert-position.java
+++ BinarySearch/Easy/search-insert-position.java
@@ -3,19 +3,38 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
-        int right = nums.length;
-        while(left < right){
+        int right = nums.length-1;
+        int ans = nums.length;
+        while(left <= right){
             int mid = (left + right) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] > target){
-                right = mid;
+            if(nums[mid] >= target){
+                ans = mid;
+                right = mid - 1;
             }
             else{
                 left = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 }
+
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+//         int left = 0;
+//         int right = nums.length;
+//         while(left < right){
+//             int mid = (left + right) / 2;
+//             if(nums[mid] == target){
+//                 return mid;
+//             }
+//             else if(nums[mid] > target){
+//                 right = mid;
+//             }
+//             else{
+//                 left = mid + 1;
+//             }
+//         }
+//         return left;
+//     }
+// }
