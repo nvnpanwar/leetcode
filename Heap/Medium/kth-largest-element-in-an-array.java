3b7d2f9b79a7819264925d3ba60254898202f5a9
--unset <nvnpanwar123@gmail.com>
2025-04-13 17:59:59 +0530

    update

--- Heap/Medium/kth-largest-element-in-an-array.java
+++ Heap/Medium/kth-largest-element-in-an-array.java
@@ -2,20 +2,50 @@
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for(int i=0;i<k;i++){
-            pq.add(nums[i]);
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for(int ele:nums){
+            max = Math.max(ele,max);
+            min = Math.min(ele,min);
         }
-        for(int i=k;i<nums.length;i++){
-            if(nums[i] > pq.peek()){
-                pq.poll();
-                pq.add(nums[i]);
+
+        int[] arr = new int[max-min+1];
+        Arrays.fill(arr,0);
+        for(int ele:nums){
+            arr[ele - min] += 1;
+        }
+        int i=max-min;
+        while(i >= 0){
+            if(arr[i] > 0){
+                k--;
+                arr[i] -= 1;
+            }
+            else{
+                i--;
+            }
+            if(k==0){
+                return i + min;
             }
         }
-        return pq.peek();
+        return 0;
     }
 }
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+//         for(int i=0;i<k;i++){
+//             pq.add(nums[i]);
+//         }
+//         for(int i=k;i<nums.length;i++){
+//             if(nums[i] > pq.peek()){
+//                 pq.poll();
+//                 pq.add(nums[i]);
+//             }
+//         }
+//         return pq.peek();
+//     }
+// }
 
 // class Solution {
 //     public int findKthLargest(int[] nums, int k) {
